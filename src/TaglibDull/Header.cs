beed6460ffd989f4b74d0b9d015403df2ae65c16
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-09-18 23:05:34 -0500

    feat(headers):  Complete `Header` constructor

--- src/TaglibDull/Header.cs
+++ src/TaglibDull/Header.cs
@@ -2,29 +2,68 @@ namespace TaglibDull;
 
 public struct Header
 {
-    public byte[] Identifier;
-    
-    /// <summary>
-    /// This should really always be 3.n and we can throw an exception
-    /// if it ends up being 2.n or 4.n or something else
-    /// </summary>
-    public byte[] Version;
-    
-    public byte Flags;
-    
     /// <summary>
     /// Header size is always 10 
     /// </summary>
     public const uint Size = 10;
 
-    public uint TagSize;
+    /// <summary>
+    /// The Identifier should always be "ID3" as we don't support any other header types
+    /// </summary>
+    public readonly static byte[] Identifier = "ID3"u8.ToArray();
+
+    /// <summary>
+    /// This should always be 3 as we're only supporting ID3v2.3.n
+    /// </summary>
+    public byte MajorVersion { get; }
+
+    /// <summary>
+    /// We don't actually care about what the revision is as they're backwards compatible,
+    /// but we do need to keep it if we're going to overwrite tag headers later
+    /// </summary>
+    public byte Revision { get; }
+
+    /// <summary>
+    /// Represents Header flags. Currently none are supported so this should always be <c>0</c>
+    /// or <c>0b00000000</c> in binary representation
+    /// </summary>
+    public byte Flags { get; }
+
+    public uint TagSize { get; }
 
     public Header(ReadOnlySpan<byte> data)
     {
-        Identifier = data[..3].ToArray();
-        Version = data[3..5].ToArray();
+        if (!data[..3].SequenceEqual(Identifier))
+        {
+            throw new FormatException("Unsupported tag format.");
+        }
+
+        // Major version should always be 3
+        if (data[3] != 3)
+        {
+            throw new FormatException("Unsupported ID3 major version.");
+        }
+
+        if (data[5] != 0)
+        {
+            throw new FormatException("Unsupported header flags.");
+        }
+
+        MajorVersion = data[3];
+        Revision = data[4];
         Flags = data[5];
         TagSize = Synchronization.ToUInt(data[6..10]) + Size;
     }
+}
 
+/// <summary>
+/// Represents the three header flags provided in ID3v2.3.n
+/// These are not currently supported so not being used anywhere.
+/// </summary>
+[Flags]
+public enum HeaderFlags
+{
+    Unsynchronization = 0b10000000,
+    ExtendedHeader = 0b010000000,
+    ExperimentalIndicator = 0b001000000
 }
