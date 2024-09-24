5a46f112a88bfc134ad40cf1da427ff937f871a4
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-09-23 22:41:36 -0500

    feat(frame-types):  Added FrameTypes enum-like static class

---
+++ src/TaglibDull/Frame/FrameTypes.cs
@@ -0,0 +1,88 @@
+namespace TaglibDull.Frame;
+
+/// <summary>
+/// Represents all declared frame types in ID3v2.3
+/// <see href="https://id3.org/id3v2.3.0#Declared_ID3v2_frames">ID3.org</see>
+/// </summary>
+/// <remarks>
+/// We do it this way instead of an Enum, because it's easier for <c>Span</c> and <c>byte[]</c> comparisons whereas
+/// enum members can only be easily converted to or cast as normal strings.
+/// </remarks>
+public static class FrameTypes
+{
+    // ReSharper disable InconsistentNaming
+    public static ReadOnlySpan<byte> AENC => "AENC"u8;
+    public static ReadOnlySpan<byte> APIC => "APIC"u8;
+    public static ReadOnlySpan<byte> COMM => "COMM"u8;
+    public static ReadOnlySpan<byte> COMR => "COMR"u8;
+    public static ReadOnlySpan<byte> ENCR => "ENCR"u8;
+    public static ReadOnlySpan<byte> EQUA => "EQUA"u8;
+    public static ReadOnlySpan<byte> ETCO => "ETCO"u8;
+    public static ReadOnlySpan<byte> GEOB => "GEOB"u8;
+    public static ReadOnlySpan<byte> GRID => "GRID"u8;
+    public static ReadOnlySpan<byte> IPLS => "IPLS"u8;
+    public static ReadOnlySpan<byte> LINK => "LINK"u8;
+    public static ReadOnlySpan<byte> MCDI => "MCDI"u8;
+    public static ReadOnlySpan<byte> MLLT => "MLLT"u8;
+    public static ReadOnlySpan<byte> OWNE => "OWNE"u8;
+    public static ReadOnlySpan<byte> PRIV => "PRIV"u8;
+    public static ReadOnlySpan<byte> PCNT => "PCNT"u8;
+    public static ReadOnlySpan<byte> POPM => "POPM"u8;
+    public static ReadOnlySpan<byte> POSS => "POSS"u8;
+    public static ReadOnlySpan<byte> RBUF => "RBUF"u8;
+    public static ReadOnlySpan<byte> RVAD => "RVAD"u8;
+    public static ReadOnlySpan<byte> RVRB => "RVRB"u8;
+    public static ReadOnlySpan<byte> SYLT => "SYLT"u8;
+    public static ReadOnlySpan<byte> SYTC => "SYTC"u8;
+    public static ReadOnlySpan<byte> TALB => "TALB"u8;
+    public static ReadOnlySpan<byte> TBPM => "TBPM"u8;
+    public static ReadOnlySpan<byte> TCOM => "TCOM"u8;
+    public static ReadOnlySpan<byte> TCON => "TCON"u8;
+    public static ReadOnlySpan<byte> TCOP => "TCOP"u8;
+    public static ReadOnlySpan<byte> TDAT => "TDAT"u8;
+    public static ReadOnlySpan<byte> TDLY => "TDLY"u8;
+    public static ReadOnlySpan<byte> TENC => "TENC"u8;
+    public static ReadOnlySpan<byte> TEXT => "TEXT"u8;
+    public static ReadOnlySpan<byte> TFLT => "TFLT"u8;
+    public static ReadOnlySpan<byte> TIME => "TIME"u8;
+    public static ReadOnlySpan<byte> TIT1 => "TIT1"u8;
+    public static ReadOnlySpan<byte> TIT2 => "TIT2"u8;
+    public static ReadOnlySpan<byte> TIT3 => "TIT3"u8;
+    public static ReadOnlySpan<byte> TKEY => "TKEY"u8;
+    public static ReadOnlySpan<byte> TLAN => "TLAN"u8;
+    public static ReadOnlySpan<byte> TLEN => "TLEN"u8;
+    public static ReadOnlySpan<byte> TMED => "TMED"u8;
+    public static ReadOnlySpan<byte> TOAL => "TOAL"u8;
+    public static ReadOnlySpan<byte> TOFN => "TOFN"u8;
+    public static ReadOnlySpan<byte> TOLY => "TOLY"u8;
+    public static ReadOnlySpan<byte> TOPE => "TOPE"u8;
+    public static ReadOnlySpan<byte> TORY => "TORY"u8;
+    public static ReadOnlySpan<byte> TOWN => "TOWN"u8;
+    public static ReadOnlySpan<byte> TPE1 => "TPE1"u8;
+    public static ReadOnlySpan<byte> TPE2 => "TPE2"u8;
+    public static ReadOnlySpan<byte> TPE3 => "TPE3"u8;
+    public static ReadOnlySpan<byte> TPE4 => "TPE4"u8;
+    public static ReadOnlySpan<byte> TPOS => "TPOS"u8;
+    public static ReadOnlySpan<byte> TPUB => "TPUB"u8;
+    public static ReadOnlySpan<byte> TRCK => "TRCK"u8;
+    public static ReadOnlySpan<byte> TRDA => "TRDA"u8;
+    public static ReadOnlySpan<byte> TRSN => "TRSN"u8;
+    public static ReadOnlySpan<byte> TRSO => "TRSO"u8;
+    public static ReadOnlySpan<byte> TSIZ => "TSIZ"u8;
+    public static ReadOnlySpan<byte> TSRC => "TSRC"u8;
+    public static ReadOnlySpan<byte> TSSE => "TSSE"u8;
+    public static ReadOnlySpan<byte> TYER => "TYER"u8;
+    public static ReadOnlySpan<byte> TXXX => "TXXX"u8;
+    public static ReadOnlySpan<byte> UFID => "UFID"u8;
+    public static ReadOnlySpan<byte> USER => "USER"u8;
+    public static ReadOnlySpan<byte> USLT => "USLT"u8;
+    public static ReadOnlySpan<byte> WCOM => "WCOM"u8;
+    public static ReadOnlySpan<byte> WCOP => "WCOP"u8;
+    public static ReadOnlySpan<byte> WOAF => "WOAF"u8;
+    public static ReadOnlySpan<byte> WOAR => "WOAR"u8;
+    public static ReadOnlySpan<byte> WOAS => "WOAS"u8;
+    public static ReadOnlySpan<byte> WORS => "WORS"u8;
+    public static ReadOnlySpan<byte> WPAY => "WPAY"u8;
+    public static ReadOnlySpan<byte> WPUB => "WPUB"u8;
+    public static ReadOnlySpan<byte> WXXX => "WXXX"u8;
+}
