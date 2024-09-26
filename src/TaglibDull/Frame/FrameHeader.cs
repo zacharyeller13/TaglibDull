593fe8c8f62ee25473048d8ad36c32cfc651a866
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-09-25 21:52:56 -0500

    feat(frames):  Implement basic FrameHeader

--- src/TaglibDull/Frame/FrameHeader.cs
+++ src/TaglibDull/Frame/FrameHeader.cs
@@ -1,4 +1,5 @@
 using System.Buffers.Binary;
+using System.Text;
 
 namespace TaglibDull.Frame;
 
@@ -11,13 +12,17 @@ public struct FrameHeader
     private readonly byte[] _frameId;
     private readonly uint _size;
     private readonly byte[] _flags;
-    
+
     public ReadOnlySpan<byte> FrameId => _frameId;
     public uint Size => _size;
     public ReadOnlySpan<byte> Flags => _flags;
 
     public FrameHeader(ReadOnlySpan<byte> data)
     {
+        if (!FrameType.IsValid(data[..4]))
+        {
+            throw new FormatException($"Read non-standard or unsupported frame type {Encoding.UTF8.GetString(data[..4])}");
+        }
         _frameId = data[..4].ToArray();
         _size = BinaryPrimitives.ReadUInt32BigEndian(data[4..8]);
         _flags = data[8..10].ToArray();
